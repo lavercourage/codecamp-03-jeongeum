156b59abd54041b618bae2254868bc123d43f265
lavercourage <smqkemfht@gmail.com>
2021-08-31 01:21:17 +0900

    new folder

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,13 +1,16 @@
 export default function BoardsNewPage(){
    
     return (
-        <div>
-            <div>게시글 등록 화면</div>
-            <div>
-            
-            </div>
-        </div>
-        
+        <Wrapper>
+            <Title>게시글 등록 화면</Title>
+            <Name>
+                
+            </Name>
+            <Substance>
+                내용<br>
+                <SubInput type="text" />
+            </Substance>
+        </Wrapper>
     )
 
 }
