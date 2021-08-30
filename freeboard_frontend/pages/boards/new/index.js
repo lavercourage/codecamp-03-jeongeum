baac4e8f09364b951d60cbc5dc54665f52a0871e
lavercourage <smqkemfht@gmail.com>
2021-08-30 14:58:24 +0900

    new folder

---
+++ freeboard_frontend/pages/boards/new/index.js
@@ -0,0 +1,13 @@
+export default function BoardsNewPage(){
+   
+    return (
+        <div>
+            <div>게시글 등록 화면</div>
+            <div>
+            
+            </div>
+        </div>
+        
+    )
+
+}
