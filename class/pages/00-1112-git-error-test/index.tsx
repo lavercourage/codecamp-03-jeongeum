7edfdc1b8811c92a348a55c0c6311074ebbe46bb
lavercourage <smqkemfht@gmail.com>
2021-11-12 15:45:35 +0900

    1112

--- class/pages/00-1112-git-error-test/index.tsx
+++ class/pages/00-1112-git-error-test/index.tsx
@@ -1,9 +1,12 @@
 export default function HiPage() {
   const fff = () => {};
 
+  const fff2 = () => {};
+
   return (
     <>
       <div onClick={fff}>hi!</div>
+      <div onClick={fff2}>hi!</div>
     </>
   );
 }
