49530be2f7584bc84746f15ced956d8116b1ac2a
lavercourage <smqkemfht@gmail.com>
2021-12-12 18:53:24 +0900

    1212 todayView

---
+++ freeboard_frontend/src/components/commons/todayView/todayView.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+const Wrapper = styled.div`
+  width: 160px;
+  height: 300px;
+`;
+
+export default function todayView() {
+  return (
+    <>
+      <Wrapper></Wrapper>
+    </>
+  );
+}
