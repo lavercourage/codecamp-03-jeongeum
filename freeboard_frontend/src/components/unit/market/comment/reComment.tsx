e26203fc23b8dca49581a6df374d90d2bfe1bf3e
lavercourage <smqkemfht@gmail.com>
2021-12-28 22:58:02 +0900

    1228 대댓글 진행시작 / 오늘본아이템 마무리하기

--- freeboard_frontend/src/components/unit/market/comment/reComment.tsx
+++ freeboard_frontend/src/components/unit/market/comment/reComment.tsx
@@ -1,7 +1,11 @@
 export default function ReCommentSolid() {
   return (
     <>
-      <div> </div>
+      <div>writer</div>
+      <div>createdAt</div>
+      <div>content</div>
+      <image>수정</image>
+      <image>삭제</image>
     </>
   );
 }
