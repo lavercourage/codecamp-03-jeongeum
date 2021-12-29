2dd848e183b3471e012c774b1c77ddd0a628f4bf
lavercourage <smqkemfht@gmail.com>
2021-12-29 23:55:29 +0900

    1229 대댓글 설계  / 오늘본아이템 마무리하기

--- freeboard_frontend/src/components/unit/market/comment/reComment.tsx
+++ freeboard_frontend/src/components/unit/market/comment/reComment.tsx
@@ -1,4 +1,8 @@
 export default function ReCommentSolid() {
+  // isComment로 대댓글 활성화 유무 정하기
+  // 내 댓글인 경우 삭제하기
+  // 남의 댓글인 경우 추가하기
+  // ㄴ 이것은 액세스 토큰 활용?
   return (
     <>
       <div>writer</div>
