84d63bc13560e154825ffb264b287d50c06a8fcd
lavercourage <smqkemfht@gmail.com>
2022-01-01 23:40:52 +0900

    0101 대댓글 설계  / 오늘본아이템 마무리하기

--- freeboard_frontend/src/components/unit/market/comment/reComment.tsx
+++ freeboard_frontend/src/components/unit/market/comment/reComment.tsx
@@ -1,10 +1,10 @@
 export default function ReCommentSolid() {
-  // isComment로 대댓글 활성화 유무 정하기
   // 내 댓글인 경우 삭제하기
   // 남의 댓글인 경우 추가하기
   // ㄴ 이것은 액세스 토큰 활용?
   // 대댓글은 컴포넌트 분리 후 합치기 or 대댓글에 적용? 재활용을 어떻게 할 것 인지?
   // 삭제할 때 alert 띄우기
+  // useState를 활용해서 대듯르을 활성화하기
   return (
     <>
       <div>writer</div>
