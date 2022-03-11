ea3c235c1ac00ad3a17fc6743063917381478968
lavercourage <smqkemfht@gmail.com>
2022-03-12 04:48:10 +0900

    220312 : [modify] 깃 테스트

--- freeboard_frontend/pages/index.tsx
+++ freeboard_frontend/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 import LandingBoard from "../src/components/unit/landing/landingBoard.container";
 
 export default function Home() {
